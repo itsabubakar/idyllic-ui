deda53c075af4da3c19186742db1f0710a684f9d
Sadiq <seeker99157@gmail.com>
2023-06-24 17:05:42 +0100

    testing a way to modify tailwindcss styles

--- src/TailwindBtn.tsx
+++ src/TailwindBtn.tsx
@@ -3,6 +3,7 @@ import React, { HTMLAttributes, ReactNode } from 'react'
 
 export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
     children: ReactNode;
+    cn: string;
 
 }
 
@@ -11,10 +12,11 @@ export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
 export const TailwindBtn: React.FC<ButtonProps> = ({
 
     children,
+    cn,
     ...props
 }) => {
     return (
-        <button className="inline-block rounded border-2 border-primary px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700" {...props}>
+        <button className={`inline-block rounded border-2 border-primary px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 ${cn && cn}`} {...props}>
             {children}
         </button>
     )
