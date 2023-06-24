e7c6a1bb4bd04cf1ef8566826aef30d9d99cebf6
Sadiq <seeker99157@gmail.com>
2023-06-24 17:13:15 +0100

    testing a way to modify tailwindcss styles again

--- src/TailwindBtn.tsx
+++ src/TailwindBtn.tsx
@@ -16,7 +16,7 @@ export const TailwindBtn: React.FC<ButtonProps> = ({
     ...props
 }) => {
     return (
-        <button className={`inline-block rounded border-2 border-primary px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 ${cn && cn}`} {...props}>
+        <button className={`inline-block rounded border-2 border-gray-600 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-gray transition duration-150 ease-in-out hover:border-gray-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-gray-600 focus:border-gray-600 focus:text-gray-600 focus:outline-none focus:ring-0 active:border-gray-700 active:text-gray-700 ${cn ? cn : ''}`} {...props}>
             {children}
         </button>
     )
