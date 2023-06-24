37985cc0dffb4bd83cd4ee9a7564c5a3eab89bc1
Sadiq <seeker99157@gmail.com>
2023-06-24 18:03:41 +0100

    mm

--- tsdx.config.js
+++ tsdx.config.js
@@ -1,19 +1,29 @@
 const postcss = require('rollup-plugin-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const tailwindcss = require('tailwindcss');
+
 module.exports = {
     rollup(config, options) {
         config.plugins.push(
             postcss({
                 plugins: [
-                    autoprefixer(),
-                    cssnano({
-                        preset: 'default',
+                    tailwindcss({
+                        purge: ['./src/**/*.tsx'],
+                        darkMode: false, // or 'media' or 'class'
+                        theme: {
+                            extend: {},
+                        },
+                        variants: {
+                            extend: {},
+                        },
+                        plugins: [],
                     }),
+                    autoprefixer(),
                 ],
                 inject: true,
                 // only write out CSS for the first bundle (avoids pointless extra files):
-                extract: !!options.writeMeta,
+                extract: false,
             })
         );
         return config;
