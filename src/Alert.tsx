609086a3c63bea155531a196033ff393d87694a4
Sadiq <seeker99157@gmail.com>
2023-07-03 17:28:58 +0100

    testing classnames

--- src/Alert.tsx
+++ src/Alert.tsx
@@ -2,14 +2,15 @@ import React from "react"
 
 export interface Alertprops {
     children: React.ReactNode,
-    type?: "success" | "error" | "info" | "warning"
+    type?: "success" | "error" | "info" | "warning",
+    className?: string
 
 }
 
-export const Alert: React.FC<Alertprops> = ({ children, type }) => {
+export const Alert: React.FC<Alertprops> = ({ children, type, className }) => {
     return (
         <>
-            <div className={`font-regular relative mb-4 block w-full rounded-lg  p-4 text-base leading-5 text-white opacity-100 ${type === "success" ? 'bg-green-500' :
+            <div className={`font-regular relative mb-4 block w-full rounded-lg  p-4 text-base leading-5 text-white opacity-100 ${type === "success" ? `bg-green-500 ${className ? className : ''}` :
                 type === "error" ? 'bg-red-500' :
                     type === "info" ? 'bg-blue-500' :
                         type === "warning" ? 'bg-orange-500' :
