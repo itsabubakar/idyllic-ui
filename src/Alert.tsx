fb5469f527aa3b3fad7ec7c4ea641c0a2ea7e447
Sadiq <seeker99157@gmail.com>
2023-07-03 19:21:47 +0100

    added spinner and alert component

--- src/Alert.tsx
+++ src/Alert.tsx
@@ -10,7 +10,7 @@ export interface Alertprops {
 export const Alert: React.FC<Alertprops> = ({ children, type, className }) => {
     return (
         <>
-            <div className={`font-regular relative mb-4 block w-full rounded-lg  p-4 text-base leading-5 text-white opacity-100 ${type === "success" ? `bg-green-500 ${className ? className : ''}` :
+            <div className={`font-regular relative block w-full rounded-lg  p-4 text-base leading-5 text-white opacity-100 ${type === "success" ? `bg-green-500 ${className ? className : ''}` :
                 type === "error" ? `bg-red-500 ${className ? className : ''}` :
                     type === "info" ? `bg-blue-500 ${className ? className : ''}` :
                         type === "warning" ? `bg-orange-500 ${className ? className : ''}` :
