f288c773f27baffff249baae86e8d6003aab66ec
Sadiq <seeker99157@gmail.com>
2023-07-06 17:09:35 +0100

    tweaked spinner

--- src/Spinner.tsx
+++ src/Spinner.tsx
@@ -11,7 +11,7 @@ export const Spinner: React.FC<Spinnerprops> = ({
 }) => {
   return (
     <div
-      className={`${color === 'dark' ? "border-gray-900" : "border-white"}   animate-spin rounded-full border-2 border-solid  border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]  ${size === 'sm' ? "w-8 h-8" : size === 'md' ? "w-10 h-10" : size === 'lg' ? "w-12 h-12" : "w-8 h-8"}`}
+      className={`${color === 'dark' ? "border-gray-900" : color === 'light' ? "border-gray-300" : "border-gray-900"}   animate-spin rounded-full border-2 border-solid  border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]  ${size === 'sm' ? "w-8 h-8" : size === 'md' ? "w-10 h-10" : size === 'lg' ? "w-12 h-12" : "w-8 h-8"}`}
       role="status">
       <span
         className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
